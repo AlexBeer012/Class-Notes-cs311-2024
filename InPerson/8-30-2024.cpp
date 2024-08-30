0aa826a0eeaddaadc61edbc159028f7c0ea26767
Alexbeer012 <111216930+AlexBeer012@users.noreply.github.com>
2024-08-30 13:12:01 -0800

    in person code start

---
+++ InPerson/8-30-2024.cpp
@@ -0,0 +1,20 @@
+// Alex Beer
+// 8/30/2024
+// CS 311 - Data Structures and Algorithms
+// University of Alaska Fairbanks
+// In person code for CS 311. Topics covered; Expression, Parameter Passing, and Operator Overloading. With extra exercises.
+
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin; 
+
+int main() {
+
+    cout << "x" << endl;
+
+    return 0;
+}
